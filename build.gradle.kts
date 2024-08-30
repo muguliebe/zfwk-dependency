376dcf21719bd33117fd58124e376b0ea7e7cb16
zany <muguliebe@gmail.com>
2024-08-30 18:43:49 +0900

    zutils 0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,7 @@ javaPlatform {
 }
 
 group = "com.egstep"
-version = "0.2.0"
-val zfwkVersion = "0.1.0"
-val zutilsVersion = "0.2.0"
+version = "0.3.0"
 
 repositories {
     mavenCentral()
@@ -27,8 +25,8 @@ dependencies {
     }
 
     //Maven BOM <dependencies> block
-    api("com.egstep:zutils-kt:${zutilsVersion}")
-    api("com.egstep:zfwk-core:${zfwkVersion}")
+    api("com.github.muguliebe:zfwk-utils:0.2.0")
+    api("com.github.muguliebe:zfwk-core:0.1.0")
     api("ch.qos.logback:logback-classic:1.4.12")
     api("org.slf4j:slf4j-api:2.0.9")
     api("org.apache.commons:commons-lang3:3.13.0")
